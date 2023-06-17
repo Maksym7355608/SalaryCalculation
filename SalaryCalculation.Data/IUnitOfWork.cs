b597581e3338cb550a575c445699d8cdab007879
Максим Чорний <maksym.chornyi@extracode.com.ua>
2023-06-17 14:10:37 +0300

    add other

---
+++ SalaryCalculation.Data/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using SalaryCalculation.Data.BaseEventModels;
+
+namespace SalaryCalculation.Data;
+
+public interface IUnitOfWork
+{
+    IMongoCollection<T> GetCollection<T>();
+    IMongoCollection<T> GetCollection<T>(string name);
+    Task PublishEventAsync<M>(M msg) where M : BusEvent;
+    Task PublishEventAsync<TBase, T>(T msg)
+        where TBase : BusEvent
+        where T : TBase;
+}
