4d1c2535de3aabbaafabe60b380cd7ed909bad0e
Максим Чорний <maksym.chornyi@extracode.com.ua>
2023-07-10 23:33:06 +0300

    Add dictionary and documents models

---
+++ Dictionary.Models/Formula.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using SalaryCalculation.Data.BaseModels;
+
+namespace Dictionary.Models;
+
+public class Formula : BaseMongoEntity<ObjectId>
+{
+    public int OrganizationId { get; set; }
+    public string Name { get; set; }
+    public string ExpressionName { get; set; }
+    public string Expression { get; set; }
+
+    public DateTime DateFrom { get; set; }
+    public DateTime? DateTo { get; set; }
+}
---
+++ Documents.Data/Entities/SimpleDocument.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using SalaryCalculation.Data.BaseModels;
+
+namespace Documents.Data.Entities;
+
+public class SimpleDocument : BaseMongoEntity<ObjectId>
+{
+    public Guid FileId { get; set; }
+    public string Name { get; set; }
+    public string FullName { get; set; }
+    public DateTime CreateDate { get; set; }
+    public int Period { get; set; }
+    public int TemplateCode { get; set; }
+    public ObjectId UserId { get; set; }
+    public string DocumentType { get; set; }
+    public int OrganizationId { get; set; }
+    public int? OrganizationUnitId { get; set; }
+    public int? PositionId { get; set; }
+    public int? EmployeeId { get; set; }
+}
