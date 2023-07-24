14a55108861cee33f84c7f7d7a93f26345894273
Максим Чорний <maksym.chornyi@extracode.com.ua>
2023-07-24 22:26:59 +0300

    update base infrastructure

---
+++ SalaryCalculation.Data/IMessageBroker.cs
@@ -0,0 +1,9 @@
+﻿using SalaryCalculation.Data.BaseModels;
+
+namespace SalaryCalculation.Data;
+
+public interface IMessageBroker
+{
+    Task PublishAsync<TMessage>(TMessage message) where TMessage : BaseMessage;
+    Task SubscribeAsync<TMessage>(string subscriptionId, Func<TMessage, Task> handler) where TMessage : BaseMessage;
+}
--- SalaryCalculation.Data/IUnitOfWork.cs
+++ SalaryCalculation.Data/IUnitOfWork.cs
@@ -5,11 +5,7 @@ namespace SalaryCalculation.Data;
 
 public interface IUnitOfWork
 {
+    IMessageBroker MessageBroker { get; }
     IMongoCollection<T> GetCollection<T>();
     IMongoCollection<T> GetCollection<T>(string name);
-    IMessageBus Bus { get; }
-    Task PublishEventAsync<M>(M msg) where M : BusEvent;
-    Task PublishEventAsync<TBase, T>(T msg)
-        where TBase : BusEvent
-        where T : TBase;
 }
