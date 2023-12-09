07e560667bc7ba369e72948c50121c888400dcad
Максим Чорний <maksym.chornyi@extracode.com.ua>
2023-12-09 16:40:58 +0200

    small update models

--- Organization.Data/BaseModels/Contact.cs
+++ Organization.Data/BaseModels/Contact.cs
@@ -1,7 +1,26 @@
+using Organization.Data.Enums;
+
 namespace Organization.Data.BaseModels;
 
 public class Contact
 {
     public int Kind { get; set; }
     public string Value { get; set; }
+
+    public Contact()
+    {
+        
+    }
+
+    public Contact(int kind, string value)
+    {
+        Kind = kind;
+        Value = value;
+    }
+
+    public Contact(EContactType kind, string value)
+    {
+        Kind = (int)kind;
+        Value = value;
+    }
 }
